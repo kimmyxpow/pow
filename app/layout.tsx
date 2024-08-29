c9f3cd8882b987db6215e523263b90a95c39faf0
envil a.k.a pow <abinovalfauzi@proton.me>
2024-08-29 11:55:23 +0700

    fix: add texture bg & change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,30 @@
-import type { Metadata } from "next";
-import { Quicksand } from "next/font/google";
-import Navbar from "@/components/navbar";
-import { Analytics } from "@vercel/analytics/react";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Raleway } from 'next/font/google';
+import Navbar from '@/components/navbar';
+import { Analytics } from '@vercel/analytics/react';
+import './globals.css';
+import Texture from '@/components/texture';
 
-const chivo = Quicksand({
-    subsets: ["latin"],
-    weight: "400",
+const sans = Raleway({
+    subsets: ['latin'],
+    weight: '400'
 });
 
 export const metadata: Metadata = {
-    title: "Pow",
+    title: 'Pow',
     description:
-        "A software engineer skilled in both backend and frontend development. i also have a good foundation in design, which helps me create functional and visually appealing applications. in my free time, i enjoy playing with cats and exploring the latest technologies :))",
+        'A software engineer skilled in both backend and frontend development. i also have a good foundation in design, which helps me create functional and visually appealing applications. in my free time, i enjoy playing with cats and exploring the latest technologies :))'
 };
 
 export default function RootLayout({
-    children,
+    children
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="en" className="scroll-smooth">
-            <body className={chivo.className}>
+            <body className={sans.className}>
+                <Texture />
                 <div className="bg-stone-200">
                     <Navbar />
                     {children}
---
+++ components/texture/index.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useRef } from 'react';
+import { useAnimationFrame } from 'framer-motion';
+
+type Props = {};
+
+const Texture = (props: Props) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useAnimationFrame((t) => {
+        const y = Math.floor(Math.random() * 5);
+        const x = Math.floor(Math.random() * 5);
+        ref.current ? (ref.current.style.transform = `translateX(${y}%) translateY(${x}%)`) : null;
+    });
+
+    return (
+        <div
+            className='pointer-events-none fixed -inset-24 z-0 bg-[url("/img/texture.png")] opacity-[0.08]'
+            ref={ref}
+        ></div>
+    );
+};
+
+export default Texture;
