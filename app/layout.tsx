11c721ba32ec126148fc6c56ce1702ad8d725b87
envil a.k.a pow <abinovalfauzi@proton.me>
2024-08-29 16:13:47 +0700

    add: speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
 import Navbar from '@/components/navbar';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 import Texture from '@/components/texture';
 
@@ -29,6 +30,7 @@ export default function RootLayout({
                     {children}
                 </div>
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
