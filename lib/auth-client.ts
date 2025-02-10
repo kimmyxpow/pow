9989f06013579e8620e745853ec77ba7b14282f4
pow <abinovalfauzi@proton.me>
2025-02-11 02:13:24 +0700

    Fix auth client baseURK

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from 'better-auth/react';
+import { env } from './env';
 
 export const authClient = createAuthClient({
-    baseURL: 'http://localhost:3000',
+    baseURL: env.NEXT_PUBLIC_APP_URL,
 });
 
 export const signIn = async () => {
