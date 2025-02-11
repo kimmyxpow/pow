6b99661fef68a9fce455927c930b742717418ddf
pow <abinovalfauzi@proton.me>
2025-02-11 18:23:24 +0700

    Move client utils to folder utils

---
+++ utils/auth-client.ts
@@ -0,0 +1,15 @@
+import { createAuthClient } from 'better-auth/react';
+
+export const authClient = createAuthClient({
+    baseURL: process.env.NEXT_PUBLIC_APP_URL!,
+});
+
+export const signIn = async () => {
+    await authClient.signIn.social({
+        provider: 'github',
+        callbackURL: '/guestbook',
+    });
+};
+
+export const signOut = async () => await authClient.signOut();
+export const useSession = () => authClient.useSession();
---
+++ utils/constants.ts
@@ -0,0 +1,158 @@
+export const navLinks = [
+    {
+        title: 'about',
+        url: '/',
+    },
+    {
+        title: 'projects',
+        url: '/projects',
+    },
+    {
+        title: 'articles',
+        url: '/articles',
+    },
+    {
+        title: 'stories',
+        url: '/stories',
+    },
+    {
+        title: 'guestbook',
+        url: '/guestbook',
+    },
+];
+
+export const projects = [
+    {
+        name: 'OpenSeries',
+        description:
+            'OpenSeries is a project that aims to help students in calculating all kinds of equations and formulas that are commonly encountered in high school.',
+        github: 'https://github.com/bellshade/OpenSeries',
+        website: 'https://www.openseries.blue',
+        tags: ['Python', 'Next.js', 'Tailwind CSS'],
+    },
+    {
+        name: '@is-it/fifteen',
+        description: '@is-it/fifteen is a lightweight, flexible utility to check if a number equals 15.',
+        github: 'https://github.com/kimmyxpow/is-it-fifteen',
+        website: 'https://15.pow.kim',
+        tags: ['Typescript', 'React.js', 'Tailwind CSS'],
+    },
+    {
+        name: 'Jolly',
+        description:
+            'Jolly makes JavaScript fun again! Write code in a playful, easy-to-read syntax and let Jolly handle the boring stuff—converting it to JavaScript.',
+        github: 'https://github.com/kimmyxpow/jolly-lang',
+        website: 'https://jolly.pow.kim',
+        tags: ['Typescript', 'Tailwind CSS', 'SvelteKit'],
+    },
+    {
+        name: 'PalettePix',
+        description:
+            'PalettePix is a web application designed to inspire and assist designers, artists, and anyone in need of color inspiration from an image.',
+        github: 'https://github.com/kimmyxpow/palettepix',
+        website: 'https://palettepix.pow.kim',
+        tags: ['React.js', 'Tailwind CSS'],
+    },
+    {
+        name: 'Nametopia',
+        description:
+            'Nametopia is a web application created to ignite creativity and aid individuals seeking name inspiration.',
+        github: 'https://github.com/kimmyxpow/nametopia',
+        website: 'https://nametopia.pow.kim',
+        tags: ['React.js', 'Tailwind CSS'],
+    },
+    {
+        name: 'BagiSkrip',
+        description: 'BagiScript is a tool for sharing codesnippets that is simple and easy to use.',
+        github: 'https://github.com/kimmyxpow/bagiskrip',
+        tags: ['Laravel', 'Tailwind CSS'],
+    },
+    {
+        name: 'Hollux',
+        description: 'Hollux is a hotel website that can be used for online reservations.',
+        github: 'https://github.com/kimmyxpow/hollux',
+        tags: ['Laravel', 'Livewire', 'Tailwind CSS', 'MySQL', 'Alpine.js'],
+    },
+];
+
+export const skills = [
+    {
+        name: 'Tailwind CSS',
+        link: 'https://tailwindcss.com',
+        icon: 'simple-icons:tailwindcss',
+    },
+    {
+        name: 'React',
+        link: 'https://react.dev',
+        icon: 'simple-icons:react',
+    },
+    {
+        name: 'Svelte',
+        link: 'https://svelte.dev',
+        icon: 'simple-icons:svelte',
+    },
+    {
+        name: 'Node.js',
+        link: 'https://nodejs.org',
+        icon: 'simple-icons:nodedotjs',
+    },
+    {
+        name: 'Bun',
+        link: 'https://bun.sh',
+        icon: 'simple-icons:bun',
+    },
+    {
+        name: 'Next.js',
+        link: 'https://nextjs.org',
+        icon: 'simple-icons:nextdotjs',
+    },
+    {
+        name: 'AdonisJS',
+        link: 'https://adonisjs.com',
+        icon: 'simple-icons:adonisjs',
+    },
+    {
+        name: 'Hono',
+        link: 'https://hono.dev',
+        icon: 'simple-icons:hono',
+    },
+    {
+        name: 'Laravel',
+        link: 'https://laravel.com',
+        icon: 'simple-icons:laravel',
+    },
+    {
+        name: 'MariaDB',
+        link: 'https://mariadb.org',
+        icon: 'simple-icons:mariadb',
+    },
+    {
+        name: 'PostgreSQL',
+        link: 'https://www.postgresql.org',
+        icon: 'simple-icons:postgresql',
+    },
+];
+
+export const experiences = {
+    'professional hustles': [
+        { company: 'Zi.Care', role: 'Fullstack Engineer', year: 'Aug 2023 – Present', img: 'zicare.png' },
+        { company: 'PT LSKK', role: 'Fullstack Developer', year: 'Jul 2022 – Dec 2022', img: 'lskk.png' },
+    ],
+    heartwork: [
+        {
+            company: 'Bellshade',
+            role: 'Open-Source Maintainer',
+            year: 'October 2021 – present',
+            img: 'bellshade.png',
+        },
+        { company: 'WPU', role: 'Community Helper', year: 'October 2021 – present', img: 'wpu.png' },
+    ],
+    'student diaries': [
+        {
+            company: 'Wikrama Vocational High School',
+            role: 'Software Engineering',
+            year: '2020 - 2023',
+            img: 'wikrama.png',
+        },
+    ],
+};
