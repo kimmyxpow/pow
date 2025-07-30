26d2ce48d1f92849d46dc0231ef63db1473f4167
Abi Noval Fauzi <noval@asi-asiapacific.com>
2025-07-30 14:48:37 +0700

    🔧 fix: improve SEO in article item

--- src/components/features/articles.svelte
+++ src/components/features/articles.svelte
@@ -40,9 +40,9 @@
 				<a
 					href="/articles/{article.slug}"
 					class="flex items-center gap-1 text-primary [&_svg]:transition-all hover:[&_svg]:-rotate-35"
-					aria-label="Read more about {article.title}"
 				>
-					Read more <Icon icon="solar:arrow-right-linear" />
+					Read more <span class="sr-only">about "{article.title}"</span>
+					<Icon icon="solar:arrow-right-linear" />
 				</a>
 			</div>
 		</div>
